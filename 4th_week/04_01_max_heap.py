507c962b5aabf542ce210812104b420fb0d512d4
chungyo <kimchungy98@gachon.ac.kr>
2025-05-22 17:00:08 +0900

    :sparkles: feat: 4주차 max_heap, dfs_recursion 구현

---
+++ 4th_week/04_01_max_heap.py
@@ -0,0 +1,26 @@
+class MaxHeap:
+    def __init__(self):
+        self.items = [None]
+
+    def insert(self, value):
+        self.items.append(value)
+        cur_index = len(self.items)- 1
+        # cur index가 1일 아닐때까지 반복
+        while cur_index != 1:
+            # 부모 노드랑 비교해서 내가 더 크면 위치 교환
+            parent_index = cur_index // 2
+            if self.items[cur_index] > self.items[parent_index]:
+                self.items[cur_index], self.items[parent_index] = self.items[parent_index], self.items[cur_index]
+                cur_index = parent_index
+            else:
+                break
+
+
+
+
+max_heap = MaxHeap()
+max_heap.insert(3)
+max_heap.insert(4)
+max_heap.insert(2)
+max_heap.insert(9)
+print(max_heap.items)  # [None, 9, 4, 2, 3] 가 출력되어야 합니다!
